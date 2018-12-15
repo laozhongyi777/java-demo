675912218c3a375dde51318c949f29ff0f0ddc01
laozhongyi7777 <qjkk88369yy@gmail>
2018-12-15 15:25:43 +0800

    22

---
+++ Jenkins/kubectl-config.sh
@@ -0,0 +1,4 @@
+kubectl config set-cluster kubernetes   --server=https://192.168.2.221:6443   --embed-certs=true   --certificate-authority=ca.pem   --kubeconfig=config
+kubectl config set-credentials cluster-admin   --certificate-authority=ca.pem   --embed-certs=true   --client-key=admin-key.pem   --client-certificate=admin.pem   --kubeconfig=config
+kubectl config set-context default --cluster=kubernetes --user=cluster-admin --kubeconfig=config
+kubectl config use-context default --kubeconfig=config
